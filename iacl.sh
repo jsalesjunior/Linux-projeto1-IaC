e0571e2b4f61d4bd5cf57fad165f63ee738262e6
jsalesjunior <jsalesjunior@gmail.com>
2022-08-13 21:35:31 -0300

    projetolinux1

---
+++ iacl.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Criando Diretórios"
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando Grupos de Usuários"
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando Usuários"
+
+useradd carlos -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_ADM
+useradd maria -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_ADM
+useradd joao -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_VEN
+useradd sebastina -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_VEN
+useradd roberto -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_SEC
+useradd amanda -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_SEC
+
+echo "Especificando Permissões aos Diretórios"
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
