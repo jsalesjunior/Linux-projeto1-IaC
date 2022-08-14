6f672e44f953fa705fd49400bf1c246bbe803dda
Joaquim S. M. Junior <35612855+jsalesjunior@users.noreply.github.com>
2022-08-13 23:10:58 -0300

    Update iacl.sh

--- iacl.sh
+++ iacl.sh
@@ -15,17 +15,17 @@ groupadd GRP_SEC
 
 echo "Criando Usuários"
 
-useradd carlos -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_ADM
-useradd maria -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_ADM
-useradd joao -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_ADM
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_ADM
 
-useradd debora -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_VEN
-useradd sebastina -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_VEN
-useradd roberto -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_VEN
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd sebastina -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_VEN
 
-useradd josefina -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_SEC
-useradd amanda -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p ${openssl passwd -crypt senha123} -G GRP_SEC
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt senha123) -G GRP_SEC
 
 echo "Especificando Permissões aos Diretórios"
 
